6c7f31507d041568e100bbf144c65552f56a4689
Zustin Mervin Paul A. Añasco <zustinanasco0@gmail.com>
2025-10-22 05:31:08 +0800

    Added a first HTML, CSS, and JS(143 nayeon)

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
-const taskInput = document.getElementById('task-input');
-const addTaskBtn = document.getElementById('add-task-btn');
-const taskList = document.getElementById('task-list');
+
+const button = document.getElementById('my-button');
 
 
-addTaskBtn.addEventListener('click', () => {
-    const taskText = taskInput.value.trim();
-
-    if (taskText !== '') {
-        
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
-            <span>${taskText}</span>
-            <button class="delete-btn">Delete</button>
-        `;
-        
-        
-        taskList.appendChild(listItem);
-
-       
-        taskInput.value = '';
-    }
-});
+const heading = document.querySelector('h1');
 
 
-taskList.addEventListener('click', (event) => {
- 
-    if (event.target.tagName === 'SPAN') {
-        event.target.parentNode.classList.toggle('completed');
-    }
-    
-  
-    if (event.target.classList.contains('delete-btn')) {
-        event.target.parentNode.remove();
-    }
+button.addEventListener('click', () => {
+
+    heading.textContent = 'You clicked the button!';
+    heading.style.color = '#7409d8ff';
 });
