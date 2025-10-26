a9107262dee1027c77685285c33dc275357c4b4b
Zustin Mervin Paul A. Añasco <zustinanasco0@gmail.com>
2025-10-26 12:03:43 +0800

    Finalize

--- script.js
+++ script.js
@@ -9,5 +9,19 @@ button.addEventListener('click', () => {
 
     heading.textContent = 'You clicked the button!';
     heading.style.color = '#7409d8ff';
+
+
+     const button = document.getElementById('my-button');
+        const audio = document.getElementById('background-music');
+
+        
+        button.addEventListener('click', () => {
+           
+            audio.play().catch(error => {
+                console.error('Error playing audio:', error);
+                alert('Unable to play music. Please check your audio file or browser settings.');
+            });
+        });
+    
 });
     
